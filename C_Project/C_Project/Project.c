98381807a45e4209114b99f540a294a06d7a553e
Wongoon <rmsgh64@gmail.com>
2021-12-02 12:45:29 +0900

    update

--- C_Project/C_Project/Project.c
+++ C_Project/C_Project/Project.c
@@ -8,48 +8,51 @@ double Sub(double x, double y);
 double Multi(double x, double y);
 double Div(double x, double y);
 double Square(double x, double y);
-double Sqrt(double x, double y);
+double Sqrt(double x);
 
 int main() {
 	char symbol;
 	double num1, num2, result;
-	int count=0;
 	Notice();
-	printf("숫자를 입력하세요\n");
+	printf("숫자를 입력하세요 : ");
 	scanf("%lf", &num1);
 	result = num1;
-	printf("\n기호를 입력하세요\n");
-	printf("+ : 더하기 - : 빼기\n* : 곱하기\n/ : 나누기\n^ : 제곱\n");
+	printf("\n+ : 더하기 - : 빼기\n* : 곱하기 / : 나누기\n^ : 제곱 r : 루트\n");
 	while (1) {
+		printf("기호를 입력하세요 : ");
 		scanf("%*c%c", &symbol);
-		if (symbol == 'r')
-			result = sqrt(result, num2);
+		if (symbol == 'r') {
+			result = Sqrt(result);
+			printf("\n결과 값 : %.4lf\n\n", result);
+			continue;
+		}
+		printf("숫자를 입력하세요 : ");
 		scanf("%lf", &num2);
 		if (symbol == '/' && num2 == 0) {
-			printf("0으로 나눌 수 없습니다");
+			printf("0으로 나눌 수 없습니다.\n");
+			printf("계산기를 종료합니다.");
 			break;
 		}
 		switch (symbol) {
-			case '+':
-				result = Add(result, num2);
-				break;
-			case '-':
-				result = Sub(result, num2);
-				break;
-			case '*':
-				result = Multi(result, num2);
-				break;
-			case '/':
-				result = Div(result, num2);
-				break;
-			case '^':
-				result = Square(result, num2);
-				break;
-			defalut:
-				printf("기호를 잘 못 입력하셨습니다.\n");
+		case '+':
+			result = Add(result, num2);
+			break;
+		case '-':
+			result = Sub(result, num2);
+			break;
+		case '*':
+			result = Multi(result, num2);
+			break;
+		case '/':
+			result = Div(result, num2);
+			break;
+		case '^':
+			result = Square(result, num2);
+			break;
+		defalut:
+			printf("기호를 잘 못 입력하셨습니다.\n");
 		}
-		printf("%.4lf\n", result);
-		count++;
+		printf("\n결과 값 : %.4lf\n\n", result);
 	}
 }
 
@@ -57,15 +60,23 @@ void Notice() {
 	int num;
 	printf("\n=============== 계산기 ===============\n\n");
 	printf("사용 방법을 보고 싶으시다면 1, 보고 싶지 않으시다면 0을 입력해주십시오.\n");
-	do {
+	while (1) {
 		scanf("%d", &num);
 		if (num == 1) {
-			printf("첫 숫자를 입력한 후 엔터를 눌러주십시오.\n");
+			printf("\n첫 숫자를 입력한 후 엔터를 눌러주십시오.\n");
 			printf("이후 나오는 기호와 숫자는 하나씩 연달아 입력 가능합니다.\n");
+			printf("단, 루트를 입력했을 시 뒤 숫자는 무시 됩니다.\n");
 			printf("입력 이후 엔터를 반드시 눌러주십시오.\n");
 			printf("나눗셈 기호 선택 후 다음 숫자가 0이면 오류가 납니다.\n\n");
+			break;
 		}
-	} while (num != 0 && num != 1);
+		else if (num == 0) {
+			break;
+		}
+		else {
+			printf("숫자를 잘 못 입력하셨습니다.\n");
+		}
+	}
 	return;
 }
 double Add(double x, double y) {
@@ -88,7 +99,7 @@ double Square(double x, double y) {
 	}
 	return num;
 }
-double sqrt(double num) {
+double Sqrt(double num) {
 	unsigned int NUM_REPEAT = 16;
 	unsigned int k;
 	double res;
